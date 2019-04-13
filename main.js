25e8aa38b55c0601585713042d2d72586469b5ba
Lei Wang <xwang@sandiego.edu>
2019-04-13 15:02:08 -0700

    added scrolling function with jQuery to contact and project links, created .css file for media queries

---
+++ main.js
@@ -0,0 +1,31 @@
+console.log('javaScript connected');
+
+function projectLinkPress() {   // event handler for project link click
+    $('nav').on('click', '.js-projects-link', function(event) {
+        event.preventDefault();
+        let targetHref = $(this).attr('href');
+        console.log('projectLinkPress ran');
+        linkPressedScroll(targetHref);
+    });
+}
+
+function contactLinkPress() {   // event handler for contact link click
+    $('nav').on('click', '.js-name-contact-link', function(event) {
+        event.preventDefault();
+        let targetHref = $(this).attr('href');
+        console.log('contactLinkPress ran');
+        linkPressedScroll(targetHref);
+    });
+}
+
+function linkPressedScroll(destination) {   // scroll to element clicked
+    console.log('linkpressedScroll ran');
+    $('html, body').animate({
+        scrollTop: $(destination).offset().top }, 500);
+}
+
+function handleStartPortfolio() {   // document ready function
+    projectLinkPress();
+    contactLinkPress();
+}
+$(handleStartPortfolio);
