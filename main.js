2eb99922c1f794bd8ff873f82f71971f24d53da4
Lei Wang <xwang@sandiego.edu>
2019-04-13 20:48:02 -0700

    finalized media queries, modified some a11y details, edited bio and quiz app description

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-console.log('javaScript connected');
+// console.log('javaScript connected');
 
 function projectLinkPress() {   // event handler for project link click
     $('nav').on('click', '.js-projects-link', function(event) {
-        event.preventDefault();
+        event.preventDefault(); // prevents default behavior of snapping to #
         let targetHref = $(this).attr('href');
-        console.log('projectLinkPress ran');
+        // console.log('projectLinkPress ran');
         linkPressedScroll(targetHref);
     });
 }
 
 function contactLinkPress() {   // event handler for contact link click
     $('nav').on('click', '.js-name-contact-link', function(event) {
-        event.preventDefault();
+        event.preventDefault(); // prevents default behavior of snapping to #
         let targetHref = $(this).attr('href');
-        console.log('contactLinkPress ran');
+        // console.log('contactLinkPress ran');
         linkPressedScroll(targetHref);
     });
 }
 
 function linkPressedScroll(destination) {   // scroll to element clicked
-    console.log('linkpressedScroll ran');
+    // console.log('linkpressedScroll ran');
     $('html, body').animate({
-        scrollTop: $(destination).offset().top }, 500);
+        scrollTop: $(destination).offset().top }, 500); // 0.5 second scroll time
 }
 
 function handleStartPortfolio() {   // document ready function
